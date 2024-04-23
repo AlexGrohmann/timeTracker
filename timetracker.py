60fc5bcd4db9903c09531847da63f37bdcf9c5d7
AlexGrohmann <alex.grohmann@googlemail.com>
2024-04-23 20:13:58 +0200

    add start timestamp

--- timetracker.py
+++ timetracker.py
@@ -1,5 +1,16 @@
 from datetime import date
+from datetime import datetime
 
 today = date.today().strftime("%d_%m_%Y")
+path = "./timetrackerdata/" + today + ".txt"
 
-f = open("./timetrackerdata/" + today + ".txt", "w")
+f = open(path, "a")
+f = open(path, "r")
+content = f.read()
+if content == "":
+    input = input("start tracking now? " + str(datetime.now()) + "\n")
+    if input == "Y":
+        f = open(path, "a")
+        f.write("start --> " + str(datetime.now()))
+f = open(path, "r")
+print(f.read())
